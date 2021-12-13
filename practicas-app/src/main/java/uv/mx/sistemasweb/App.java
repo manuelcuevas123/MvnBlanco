22b7db867f01cca6cf939f791ea1aeeeef1e636e
manuelcuevas123 <85857835+manuelcuevas123@users.noreply.github.com>
2021-12-13 13:33:06 -0600

    calificacion

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Manuel Cuevas quiere 8 porfa, dios se apiade de mi alma :3" );
     }
 }
